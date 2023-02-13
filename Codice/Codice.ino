7c5ab77f93eea5695dedc5d10ea44dc26d432cfa
maurobortolani <mauro.bortolani@ialedu.it>
2023-02-13 16:25:12 +0100

    Update Codice.ino

--- Codice/Codice.ino
+++ Codice/Codice.ino
@@ -15,22 +15,31 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
   Serial.begin(115200);
+  initDisplay();
+  messaggioBenvenuto();  
+}
 
+void loop() {
+
+
+  
+  
+}
+
+void initDisplay() {
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
   delay(2000);
   display.clearDisplay();
+}
 
+void messaggioBenvenuto() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 10);
   // Display static text
   display.println("Hello, world!");
   display.display(); 
-}
-
-void loop() {
-  
 }
