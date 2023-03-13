6da083b849bf86ef1a9a24490e141aa3e103db30
maurobortolani <mauro.bortolani@ialedu.it>
2023-03-13 16:38:01 +0100

    Add CO2

--- Codice/Codice.ino
+++ Codice/Codice.ino
@@ -4,33 +4,48 @@
 #include "DHT.h"
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
+#include <SoftwareSerial.h>
 
 #define VERSION "1.0"
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 #define DHTPIN 0
 #define DHTTYPE DHT22
 
-#define BMP_SCK  (13)
+#define BMP_SCK (13)
 #define BMP_MISO (12)
 #define BMP_MOSI (11)
-#define BMP_CS   (10)
+#define BMP_CS (10)
+
+#define sensor s_serial
+
 
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 DHT dht(DHTPIN, DHTTYPE);
 Adafruit_BMP280 bmp;
+SoftwareSerial s_serial(14, 12);  // RX, TX
 
+
+const unsigned char cmd_get_sensor[] = {
+  0xff, 0x01, 0x86, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x79
+};
+unsigned char dataRevice[9];
+int temperature;
+int CO2PPM;
+
+int count = 0;
 
 void setup() {
+  sensor.begin(9600);
   Serial.begin(115200);
   dht.begin();
   initBMP280();
   initDisplay();
-  messaggioBenvenuto();  
-
+  messaggioBenvenuto();
 }
 
 void loop() {
@@ -42,20 +57,26 @@ void loop() {
   if (isnan(h) || isnan(t) || isnan(p) || isnan(a)) {
     Serial.println(F("Failed to read from sensor!"));
     //return;
-  }else
-  {
+  } else {
     printSensor(h, t, p, a);
   }
 
+  if (dataRecieve()) {
+    Serial.print("CO2: ");
+    Serial.print(CO2PPM);
+    Serial.println("ppm");
+  }
 
+  display.println(count++, DEC);
+  display.display();
   delay(2000);
-  
 }
 
 void initDisplay() {
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
+    for (;;)
+      ;
   }
   delay(2000);
   display.clearDisplay();
@@ -66,44 +87,46 @@ void initDisplay() {
 void messaggioBenvenuto() {
   display.setCursor(0, 10);
   // Display static text
-  display.print("Che Aria\nChe Tira\n");
+  display.print("Che Aria Che Tira ");
   display.println(VERSION);
-  display.display(); 
+  display.display();
   delay(3000);
   display.clearDisplay();
 }
 
-void printSensor(float h, float t, float p, float a)
-{
+void printSensor(float h, float t, float p, float a) {
   display.setCursor(0, 0);
   display.clearDisplay();
   display.print("Umidita': ");
   display.print(h);
   display.println(" %");
-  display.println();
+  //display.println();
   display.print("Temperatura: ");
   display.print(t);
   display.println(" C");
-  display.println();
+  //display.println();
   display.print("Pressione: ");
-  display.print(p/100000, 3);
-  display.println(" Bar"); 
-  display.println();
+  display.print(p / 100000, 3);
+  display.println(" Bar");
+  //display.println();
   display.print("Altitudine: ");
   display.print((int)(a));
-  display.print(" mt"); 
-  display.println();
-  display.display(); 
-  delay(2000);
+  display.println(" mt");
+
+  display.print("CO2: ");
+  display.print(CO2PPM);
+  display.println(" ppm");
+  //display.println();
+  display.display();
 }
 
-void initBMP280()
-{
+void initBMP280() {
   unsigned status = bmp.begin(0x76);
   if (!status) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                      "try a different address!"));
-    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+                     "try a different address!"));
+    Serial.print("SensorID was: 0x");
+    Serial.println(bmp.sensorID(), 16);
     Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
     Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
     Serial.print("        ID of 0x60 represents a BME 280.\n");
@@ -116,4 +139,64 @@ void initBMP280()
                     Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                     Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
   }
+}
+
+bool dataRecieve(void) {
+  byte data[10] = {0,0,0,0,0,0,0,0,0, 0};
+  int i = 0;
+  //transmit command data
+  for (i = 0; i < sizeof(cmd_get_sensor); i++) {
+    sensor.write(cmd_get_sensor[i]);
+  }
+  delay(10);
+  //begin reveiceing data
+  if (sensor.available()) {
+    data[9] = 0x55;
+    int i = 0;
+    while (sensor.available()) {
+      data[i] = sensor.read();
+      
+      if(i > 0)
+      {
+        if(data[0] == 0xff && data[1] == 0x86)
+        {
+          i++;
+        }
+        else
+        {
+          i = 1;
+          data[0] = data[1];
+          data[1] = 0;
+        }
+      }
+      else
+      {
+        if (data[0] == 0xFF) i++;
+      }
+    }
+  }
+
+  //printRX(data);
+
+  Serial.println("");
+  /*
+  if ((i != 9) || (1 + (0xFF ^ (byte)(data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7]))) != data[8]) {
+    return false;
+  }
+  */
+  CO2PPM = (int)data[2] * 256 + (int)data[3];
+  temperature = (int)data[4] - 40;
+  return true;
+}
+
+void printRX(byte *data)
+{
+  Serial.println("Start\tCommand\tHigh\tLow\tB4\tB5\tB6\tB7\tChs\tTest\tsum");
+  for(int i = 0; i < 10; i++)
+  {
+    Serial.print(data[i], HEX);
+    Serial.print("\t");
+  }
+  Serial.print(1 + (0xFF ^ (byte)(data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7])), HEX);
+  Serial.println();
 }
