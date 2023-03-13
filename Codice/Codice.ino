7950417ce3393edc65bae48d593cf225132a90fd
maurobortolani <mauro.bortolani@ialedu.it>
2023-03-13 11:10:21 +0100

    Add DHT22

--- Codice/Codice.ino
+++ Codice/Codice.ino
@@ -1,28 +1,42 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
-*********/
-
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "DHT.h"
+
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define DHTPIN 0
+#define DHTTYPE DHT22
+#define VERSION "1.0"
+
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+DHT dht(DHTPIN, DHTTYPE);
+
 
 void setup() {
   Serial.begin(115200);
+  dht.begin();
   initDisplay();
   messaggioBenvenuto();  
+
 }
 
 void loop() {
-
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }else
+  {
+    printDHT();
+  }
 
   
+  delay(2000);
   
 }
 
@@ -40,6 +54,24 @@ void messaggioBenvenuto() {
   display.setTextColor(WHITE);
   display.setCursor(0, 10);
   // Display static text
-  display.println("Hello, world!");
+  display.print("Che Aria Che Tira ");
+  display.println(VERSION);
+  display.display(); 
+  delay(1000);
+  display.clearDisplay();
+}
+
+void printDHT()
+{
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.clearDisplay();
+  display.print("Humidity: ");
+  display.print(h);
+  display.println("%");
+  display.print("Temperature: ");
+  display.print(t);
+  display.println("C");
   display.display(); 
 }
