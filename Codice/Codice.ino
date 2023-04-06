cd4cbaac70521a280c1ab1d305d8fffce2fa9c11
maurobortolani <mauro.bortolani@ialedu.it>
2023-04-06 10:19:24 +0200

    pulizia codice

--- Codice/Codice.ino
+++ Codice/Codice.ino
@@ -5,6 +5,8 @@
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
 #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
 #define VERSION "1.0"
 
@@ -37,6 +39,92 @@ int CO2PPM;
 
 int count = 0;
 
+// Update these with values suitable for your network.
+
+const char* ssid = "Polo_Pubblica";
+const char* password = "";
+const char* mqtt_server = "broker.hivemq.com";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(100)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  display.setCursor(0, 0);
+  display.clearDisplay();
+  display.print("Connecting to ");
+  display.println(ssid);
+  display.display();
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    display.print(".");
+    display.display();
+  }
+
+  randomSeed(micros());
+
+  display.println("");
+  display.println("WiFi connected");
+  display.println("IP address: ");
+  display.println(WiFi.localIP());
+  display.display();
+  delay(2000);
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      //client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("cheariachetirain");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void setup() {
   sensor.begin(9600);
   Serial.begin(115200);
@@ -44,6 +132,9 @@ void setup() {
   initBMP280();
   initDisplay();
   messaggioBenvenuto();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop() {
@@ -58,6 +149,21 @@ void loop() {
     //return;
   } else {
     printSensor(h, t, p, a, c);
+  }
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "data:#%ld,um:#%0.2f%%,T:%0.2f°C,p:%0.3fBar,a:%0.2fmt,c:%dppm", value, h, t, p/100000, a, c);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("cheariachetiraout", msg);
   }
 
   delay(2000);
